57438e86d9a3426d612eba91099fe6afd12fbed6
marco <marco.basso99@gmail.com>
2024-12-18 17:46:48 +0100

    rollback

--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @menu_items = MenuItem.all
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,3 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_18_111247) do
-  create_table "menu_items", force: :cascade do |t|
-    t.string "name"
-    t.string "url"
-    t.integer "parent_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["parent_id"], name: "index_menu_items_on_parent_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.string "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "menu_items", "parents"
-end
